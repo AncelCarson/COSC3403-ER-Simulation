b7948454d0da5b8d604485ffc5c34b9d3b5b563f
AncelCarson <ancel.carson@gmail.com>
2019-03-29 12:45:04 -0500

    Added comments and methods

--- HospitalSimulation/Patient.cs
+++ HospitalSimulation/Patient.cs
@@ -2,11 +2,16 @@
 
 public class Patient
 {
-    private int rating, roomTime;
+    private int 
+        rating, 
+        roomTime,
+        arrivalTime,
+        waitLength;
     private static int totalPatientNum;
     private float delayTime;
     Random rnd = new Random();
 
+    //Default constructor 
     public Patient()
 	{
         rating = 2;
@@ -14,6 +19,7 @@ public class Patient
         totalPatientNum++;
 	}
 
+    //Constructor used to crete random patient
     public Patient(int[] severityRatings, int[] roomTimes, float[] waitDelays)
     {
         SetRating(severityRatings);
@@ -21,21 +27,38 @@ public class Patient
         SetDelayTime(waitDelays);
     }
 
+    //returns rating of patient
     public int GetRating()
     {
         return rating;
     }
 
+    //returns time gap between patients
     public float GetDelayTime()
     {
         return delayTime;
     }
 
+    //returns patient number by when it arrived
     public int GetPatientNum()
     {
         return totalPatientNum;
     }
 
+    //sets the time the patient arrived at the hospital
+    public void SetArrivalTime(int localTime)
+    {
+        arrivalTime = localTime;
+    }
+
+    //Determines how long a patient waited and returns how long it will be in a room
+    public int Roomed(int localTime)
+    {
+        waitLength = localTime - arrivalTime;
+        return roomTime;
+    }
+
+    //randomly assigns a rating to the patient
     private void SetRating(int[] severityRatings)
     {
         int rand = rnd.Next(1, 100);
@@ -50,6 +73,7 @@ public class Patient
         }
     }
 
+    //Assigns appropriate room time with a chance of increased time
     private void SetRoomTime(int[] roomTimes)
     {
 
@@ -76,6 +100,7 @@ public class Patient
             roomTime += (int)((float)roomTime * ((float)rnd.Next(100) / 200.0));   //Add up to 50% more time
     }
 
+    //randomly assigns time gap between patients
     private void SetDelayTime(float[] waitDelays)
     {
         float totalDelay = waitDelays[1] - waitDelays[0];
